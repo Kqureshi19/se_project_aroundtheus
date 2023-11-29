2c83d93f55d7965dc23c912b1de2c6f50a2a704b
Kqureshi19 <Kqureshi3@yahoo.com>
2023-11-28 20:38:42 -0500

    Update 11282023

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,9 +2,21 @@
 // pass all the settings on call
 //spread operator is the same as array.from with few very minor differences
 
-function showInputError(formElement, inputElement, {inputErrorClass,errorClass}) {
+/* -------------------------------------------------------------------------- */
+/*                                  Functions                                 */
+/* -------------------------------------------------------------------------- */
+//Step 1-Validate the 'Edit Profile" Form
+//Step 2--Validate the "New Place" Form
+
+function showInputError(
+  formElement,
+  inputElement,
+  { inputErrorClass, errorClass }
+) {
   //console.log(inputElement.id);
   //console.log("#" + inputElement.id + "-error");
+  //we're searching for the span that contains the error, don't haev to use formElement
+  //template literals allow us to put variables inside of it and string
   const errorMessageElement = formElement.querySelector(
     `#${inputElement.id}-error`
   );
@@ -12,21 +24,64 @@ function showInputError(formElement, inputElement, {inputErrorClass,errorClass})
   inputElement.classList.add(inputErrorClass);
   errorMessageElement.textContent = inputElement.validationMessage;
   errorMessageElement.classList.add(errorClass);
+}
 
-
+function hideInputError(
+  formElement,
+  inputElement,
+  { inputErrorClass, errorClass }
+) {
+  //console.log(inputElement.id);
+  //console.log("#" + inputElement.id + "-error");
+  const errorMessageElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  //console.log(errorMessageElement);
+  inputElement.classList.remove(inputErrorClass);
+  errorMessageElement.textContent = "";
+  errorMessageElement.classList.remove(errorClass);
+}
+//will actually check the validity of the input
+//will run everytime a user puts an input into the form
 function checkInputValidity(formElement, inputElement, options) {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, options);
-  } else {
-    hideInputError(formElement, inputElement, options);
+    return showInputError(formElement, inputElement, options);
   }
+
+  hideInputError(formElement, inputElement, options);
+}
+
+//look through all the input elements and check the validity of all of them
+//if at least 1 is invalid, turn off/disable the button
+function toggleButtonState(
+  inputElements,
+  submitButton,
+  { inactiveButtonClass }
+) {
+  let foundInvalid = false;
+  inputElements.forEach((inputElement) => {
+    if (!inputElement.validity.valid) {
+      foundInvalid = true;
+    }
+  });
+
+  if (foundInvalid) {
+    submitButton.classList.add(inactiveButtonClass);
+    return (submitButton.disabled = true);
+  }
+
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
 }
 
 //e.target is the same as inputElement
+//loop through all the inputs and listen to an event
+//also input event listener,,also a change one to research
 function setEventListeners(formElement, options) {
   const { inputSelector } = options; //these 2 lines are the same just different ways of writing it
   //const inputSelector = options.inputSelector;
   const inputElements = [...formElement.querySelectorAll(inputSelector)];
+  const submitButton = formElement.querySelector(".modal__save-button");
   inputElements.forEach((inputElement) => {
     inputElement.addEventListener("input", (e) => {
       //console.dir(inputElement);
@@ -34,9 +89,12 @@ function setEventListeners(formElement, options) {
       //the error mesage is on the validationMessage, it is a top level property
       //console.log(inputElement.validationMessage);
       checkInputValidity(formElement, inputElement, options);
+      toggleButtonState(inputElements, submitButton, options);
     });
   });
 }
+//spread operator workst the same way as array.from,
+//loop through all the buttons and add event listeners and prevent reloading the page
 function enableValidation(options) {
   const formElements = [...document.querySelectorAll(options.formSelector)];
   //console.log(formElements);
@@ -46,10 +104,10 @@ function enableValidation(options) {
     });
 
     setEventListeners(formElement, options);
+    //Pseudo Code:
     // look for all inputs inside of form
     //loop through all the inputs to see if all are valid
-    // if  input is not valid,
-    //grab the validation message
+    // if  input is not valid, grab the validation message
     //add error classs to input (make it red)
     //display error message
     //disable button
@@ -60,12 +118,62 @@ function enableValidation(options) {
 }
 
 const config = {
-  formSelector: ".modal__form", // .popup__form
-  inputSelector: ".modal__input", //.popup__input
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save-button",
+  inactiveButtonClass: "modal__save-button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
 };
 
 enableValidation(config);
+
+/* -------------------------------------------------------------------------- */
+/*                Step 3-Closing the Popup  By Clicking on the Overlay        */
+/* -------------------------------------------------------------------------- */
+
+function foundModalOpened(evt, modal) {
+  if (evt.target.classList.contains("modal_opened")) {
+    closeModal(modal);
+  }
+}
+
+///Step 3- Preview Card Modal Closing
+previewCardModal.addEventListener("click", function (evt) {
+  console.log("Preview card image ");
+  console.log(evt.target.classList);
+  //   if (evt.target.classList.contains("modal_opened")) {
+  //     closeModal(previewCardModal);
+  //   }
+
+  foundModalOpened(evt, previewCardModal);
+});
+
+///Step 3-Profile Edit Modal Closing
+
+profileEditModal.addEventListener("click", function (evt) {
+  foundModalOpened(evt, profileEditModal);
+});
+
+///Step 3-Profile Add Modal Closing
+//console.log(profileEditModal);
+addCardModal.addEventListener("click", function (evt) {
+  foundModalOpened(evt, addCardModal);
+});
+
+/* -------------------------------------------------------------------------- */
+/*           Step 4-Closing the popup/modal by Pressing Esc Key button        */
+/* -------------------------------------------------------------------------- */
+function handleEsc(e) {
+  //console.log("You are pressing a button!!");
+  if (e.key === "Escape") {
+    console.log("You are pressing a the escape button!!");
+    const modal = document.querySelector(".modal_opened");
+    closeModal(modal);
+  }
+}
+/* -------------------------------------------------------------------------- */
+/*                               Event Listeners                              */
+/* -------------------------------------------------------------------------- */
+
+document.addEventListener("keydown", handleEsc);
