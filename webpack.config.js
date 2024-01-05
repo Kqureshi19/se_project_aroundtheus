e4e99a139620082ac58f98911847e466bf34b293
Kqureshi19 <Kqureshi3@yahoo.com>
2024-01-04 19:38:17 -0500

    Update 1242024v1.1

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      favicon: "./src/images/jacques-cousteau.jpg", // path to the favicon
+      favicon: "./src/images/favicon.ico", // path to the favicon
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
